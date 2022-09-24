1577ee89917eafe52e02bc9e9a1ff063851de382
hebaashraf21 <heba.elmaaboud00@eng-st.cu.edu.eg>
2022-09-24 09:22:23 +0200

    login layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/modules/Login/LoginScreen.dart';
 import 'package:shop/modules/onBoarding.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: onBoardingScreen(),
+      home: LoginScreen(),
     );
   }
 }
--- lib/modules/Login/LoginScreen.dart
+++ lib/modules/Login/LoginScreen.dart
@@ -1,11 +1,69 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shop/shared/components/components.dart';
 
 class LoginScreen extends StatelessWidget
 {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+
+    var EmailController=TextEditingController();
+    var PasswordController=TextEditingController();
+
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(padding: EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Login",style: TextStyle(fontFamily: 'Cairo-Black',fontSize: 40),),
+          Text("Login now to browse our hot offers",style: TextStyle(fontFamily: 'Cairo-Black',fontSize: 20,color: Colors.grey),),
+          SizedBox(height:30),
+          defaultTextFormField(
+            controller: EmailController,
+             type: TextInputType.emailAddress,
+              validate: (val){
+                if(val.isEmpty)
+                {
+                  return "Please enter your E-mail.";
+                }
+              },
+               label: "Email",
+                prefix: Icons.email),
+
+                SizedBox(height:10),
+
+                defaultTextFormField(
+                  controller: EmailController,
+                   type: TextInputType.visiblePassword,
+                    validate: (val){
+                      if(val.isEmpty)
+                      {
+                        return "Please enter your password.";
+                      }
+                    },
+                     label: "Password",
+                      prefix: Icons.lock_outline,
+                      suffix: Icons.remove_red_eye_outlined,
+                      suffixPressed: (){}),
+
+                      SizedBox(height:20),
+
+                      defaultButton(function: (){}, text: "Login"),
+
+                      SizedBox(height:10),
+
+                      Row(
+                        children: [
+                          Text("Don/'t have an account?"),
+                          defaultTextButton(ontap: (){}, text: 'Register now!'),
+                        ],
+                      ),
+        ],
+      ),
+      )
+    );
     throw UnimplementedError();
   }
 
